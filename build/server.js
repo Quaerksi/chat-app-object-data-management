79da3a7f0fee5763bb22f5048d619b03ac39f364
Juliette <juliettesalevsky@gmail.com>
2022-05-09 13:05:59 +0200

    new translatet ts files in build

--- build/server.js
+++ build/server.js
@@ -7,8 +7,6 @@ const express_1 = __importDefault(require("express"));
 const socket_io_1 = require("socket.io");
 const http_1 = __importDefault(require("http"));
 require('dotenv').config();
-// import dotenv from 'dotenv' //not imported yet
-// think about git with git.ignore
 const port = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.static("public"));
@@ -23,22 +21,20 @@ app.get('/', (req, res) => {
     res.render('welcome');
 });
 app.get('/index/user/:user', (req, res) => {
-    // console.log(`In index ${req.params.user}`)
     if (users[req.params.user] == null) {
         return res.redirect('/');
     }
     res.render('index', { rooms: rooms, user: req.params.user });
 });
 app.post('/index', (req, res) => {
-    // console.log(`Check new user name ${req.body.newUser}`);
     if (users[req.body.newUser] != null) {
         return res.redirect('/');
     }
     users[req.body.newUser] = {};
+    io.emit('user names', Object.keys(users));
     return res.render('index', { rooms: rooms, user: req.body.newUser });
 });
 app.get('/room/:room/user/:user', (req, res) => {
-    // console.log(`get room: ${users[req.params.user]}, ${rooms.includes(req.params.room)}`);
     if (users[req.params.user] == null || !rooms.includes(req.params.room)) {
         return res.redirect('/');
     }
@@ -46,17 +42,13 @@ app.get('/room/:room/user/:user', (req, res) => {
 });
 //has to be fixed create room
 app.post('/room/:room/user/:user', (req, res) => {
-    console.log(`Room ${req.body.roomName}, User ${req.body.roomUser}`);
-    // if (Object.keys(rooms).includes(req.body.roomName)) {
+    //  console.log(`Room ${req.body.roomName}, User ${req.body.roomUser}`)
     if (rooms.includes(req.body.roomName)) {
         // return res.redirect('/index');
         return res.render('index', { rooms: rooms, user: req.body.roomUser });
     }
-    // rooms[req.body.roomName] = {'user': {}};
     rooms.push(req.body.roomName);
     res.render('room', { room: req.body.roomName, user: req.body.roomUser });
-    // console.log(`create: ${JSON.stringify(rooms[req.body.roomName])}`)
-    // res.redirect(`${req.body.roomName}`);
     io.emit('new room created', req.body.roomName);
 });
 //  ***********************function*************************************************************************
@@ -76,7 +68,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         let index2;
         let index1 = Object.values(users).findIndex((object) => {
-            // console.log(`disconnect 2: ${JSON.stringify(object)}`)
+            //find right entry -> SocketId for room
             let find = Object.values(object).find((mySocket, i, arr) => {
                 // console.log(`MySocket: ${JSON.stringify(Object.keys(mySocket))}, ${mySocket['id']}`);
                 if (mySocket['id'] == socket.id) {
@@ -89,16 +81,16 @@ io.on('connection', (socket) => {
             });
             return find == null ? false : true;
         });
-        console.log(`Disconnect: index1 = ${index1}, index2 = ${index2}`);
+        // console.log(`Disconnect: index1 = ${index1}, index2 = ${index2}`);
+        //if socket disconnect is from a chat room, then delete entry and send message to the remaining chat members
         if (typeof Object.values(users)[index1] !== 'undefined') {
             const name = Object.keys(users)[index1];
             if (typeof Object.values(users[name])[index2] !== 'undefined') {
                 const room = Object.keys(users[name])[index2];
                 socket.to(room).emit('chat message', `${name} left`);
                 socket.leave(socket);
-                console.log(`Leave  ${room}, ${name}`);
+                // console.log(`Leave  ${room}, ${name}`)
                 delete users[name][room];
-                // console.log(`(Join) ${room}, ${name}`)
             }
             else {
                 console.log(`Error in index2`);
@@ -109,23 +101,18 @@ io.on('connection', (socket) => {
         }
     });
     //handle new user connections
-    // socket.emit('new member', room, user);
     socket.on('new chat member', (room, user) => {
-        // console.log(`user ${user}, room ${room}`)
         //if user enters a chat second time
         if (typeof users[user][room] !== 'undefined') {
-            // console.log(`Delete if new enter: ${users[user]} + ${users[user][room]} + ${typeof users[user][room]}`);
+            socket.to(room).emit('chat message', `${user} left`);
             users[user][room].emit('chat message', `${user} connected again. You got disconnected`);
             //leave with old socket
             users[user][room].leave(room);
-            // users[user][room].emit('chat message', `${user} connected again. You got disconnected`)
-            // socket.to(room).emit('chat message', `${user} connected again. You got disconnected`)
-            console.log(`Join ${room}, ${user}`);
         }
         //enter with new socket
         users[user][room] = socket;
         socket.join(room);
-        console.log(`Join ${room}, ${user}`);
+        // console.log(`Join ${room}, ${user}`)
         //send message to chat members
         socket.to(room).emit('chat message', `${user} has joined`);
         //who is online, send to socket
@@ -133,6 +120,9 @@ io.on('connection', (socket) => {
         usersInRoomOnline.length > 1
             ? socket.emit('chat message', `Online: ${usersInRoom(room).filter(name => name != user).toString()}`)
             : socket.emit('chat message', `You are alone`);
+    });
+    socket.on('send user names', () => {
+        socket.emit('user names', Object.keys(users));
     });
     //broadcast messages  
     socket.on('chat message', (room, user, msg) => {
